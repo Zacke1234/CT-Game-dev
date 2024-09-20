8668655c16376d3e9b9775d51dc0df2c0c666aa3
Zacke1234 <zakariashager@gmail.com>
2024-09-20 14:00:55 +0200

    test2

--- Assets/Scripts/Enemy/EnemyAuthoring.cs
+++ Assets/Scripts/Enemy/EnemyAuthoring.cs
@@ -6,12 +6,7 @@ using Unity.Mathematics;
 
 public class EnemyAuthoring : MonoBehaviour
 {
-    //public GameObject prefab;
-    //public float speed;
-    //public float health;
-    //public float damageOutput;
-    //static public Vector3 target;
-    //public GameObject Player;
+    
 
     public float SpawnCooldown = 0.5f;
     public List<EnemySC> EnemiesSC;
@@ -24,9 +19,6 @@ public class EnemyAuthoring : MonoBehaviour
 
             Entity enemyEntity = GetEntity(TransformUsageFlags.None);
 
-            //AddComponent<FireEnemyTag>(enemyEntity);
-            //SetComponentEnabled<FireEnemyTag>(enemyEntity, false);
-            //AddComponent<EnemyTag>(enemyEntity);
 
             AddComponent(enemyEntity, new EnemySpawnerComp
             {
@@ -50,56 +42,9 @@ public class EnemyAuthoring : MonoBehaviour
 
             AddComponentObject(enemyEntity, new EnemyDTCONT {  enemies = enemyData });
 
-            //AddComponent(enemyEntity, new EnemyPrefab 
-            //{
-            //    Value = GetEntity(authoring.prefab, TransformUsageFlags.Dynamic) }
-            //);
-
-            ////AddComponent(enemyEntity, new EnemySpeed
-            ////{
-            ////    Value = authoring.speed
-            ////}) ;
-
-            ////AddComponent(enemyEntity, new EnemyHealth
-            ////{
-            ////    Value = authoring.health
-            ////});
-
-            ////AddComponent(enemyEntity, new EnemyDamageOutput
-            ////{
-            ////    Value = authoring.damageOutput
-            ////});
+            
         }
     }
 
-    //public struct EnemyDTContainer : IComponentData
-    //{
-    //    public float value;
-    //}
-
-    //public struct EnemySpeed : IComponentData
-    //{
-    //    public float Value;
-    //}
-
-    //public struct EnemyHealth : IComponentData 
-    //{
-    //    public float Value;
-    //}
-
-    //public struct EnemyDamageOutput : IComponentData 
-    //{
-    //    public float Value;
-    //}
-    //public struct EnemyPrefab : IComponentData
-    //{
-    //    public Entity Value;
-    //}
-
-    //public struct FireEnemyTag : IComponentData, IEnableableComponent
-    //{
-
-    //}
-    //public struct EnemyTag : IComponentData 
-    //{ }
+    
 }
--- Assets/Scripts/Enemy/EnemyMoveSystem.cs
+++ Assets/Scripts/Enemy/EnemyMoveSystem.cs
@@ -13,12 +13,11 @@ public partial struct EnemyMoveSystem : ISystem
     private EntityManager entityManager;
     private Entity playerEntity;
 
-    //Vector3 target;
     
     [BurstCompile]
     public void OnStartRunning(ref SystemState state)
     {
-        //target = new Vector2 (5, 5);
+
     }
 
     [BurstCompile]
@@ -28,13 +27,8 @@ public partial struct EnemyMoveSystem : ISystem
         entityManager = state.EntityManager;
         playerEntity = SystemAPI.GetSingletonEntity<PlayerAuthoring.PlayerPrefab>();
 
-        foreach (var (enemyComponent, transformComponent) in SystemAPI.Query<EnemyComponent, RefRW<LocalTransform>>())
+        foreach (var (enemyComponent, transformComponent) in SystemAPI.Query<EnemyComponent, RefRW<LocalTransform>>()) 
         {
-            //transform.ValueRW.Position += transform.ValueRO.Up() * moveSpeed.Value * deltatime;
-            //Debug.Log("move!");
-            //transform.ValueRW.Position = Vector3.MoveTowards(transform.ValueRW.Position, target , moveSpeed.Value * deltatime); ;
-            //transform.ValueRW.Position += transform.ValueRO.Up() * moveSpeed.Value * deltatime;
-
             float3 direction = entityManager.GetComponentData<LocalTransform>(playerEntity).Position - transformComponent.ValueRO.Position;
             float angle = math.atan2(direction.y, direction.x) + math.radians(90);
             transformComponent.ValueRW.Rotation = quaternion.Euler(new float3(0,0, angle));
